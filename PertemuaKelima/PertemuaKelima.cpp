ac92b7ccf15baed9d1088d4e705f23a93dfe6b08
Davinn354 <daviipad53@gmail.com>
2024-03-25 14:24:45 +0700

    Add project files.

---
+++ PertemuaKelima/PertemuaKelima.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input()
+{
+    cout << "Masukan Bilangan Pertama = ";
+    cin >> bilSatu;
+    cout << "Masukan Bilangan Kedua = ";
+    cin >> bilDua;
+}
+
+float penjumlahan(float a, float b)
+{
+    return a + b;
+}
+
+float pembagian(float a, float b)
+{
+    return a / b;
+}
+
+float penkalian(float a, float b)
+{
+    return a * b;
+}
+
+float pengurangan(float a, float b)
+{
+    return a - b;
+}
